f97a52415870b467b10bd7315371107ae60991e8
Oli Wennell <oli.wennell@gmail.com>
2019-05-10 12:42:12 +0100

    Initial commit

---
+++ autofac-example/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Autofac;
+
+namespace autofac_example
+{
+    public class Program
+    {
+        public void Go()
+        {
+            var container = ConfigureAutofac();
+
+            DoThings(container);
+        }
+
+        private IContainer ConfigureAutofac()
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterType<RealLogger>().As<ILogThings>();
+            return containerBuilder.Build();
+        }
+
+        private void DoThings(IContainer container)
+        {
+            var logger = container.Resolve<ILogThings>();
+            logger.Log("hello world");
+        }
+    }
+
+    public interface ILogThings
+    {
+        void Log(string message);
+    }
+
+    public class RealLogger : ILogThings
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ tests/Test.cs
@@ -0,0 +1,15 @@
+﻿using autofac_example;
+using NUnit.Framework;
+
+namespace tests
+{
+    public class Test
+    {
+        [Test]
+        public void Call_things()
+        {
+            var program = new Program();
+            program.Go();
+        }
+    }
+}
